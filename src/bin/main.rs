ed6588af1af3ca203a50de6c54e9bf77a4a6eac8
Faith <faithkamau892@gmail.com>
2025-06-10 17:34:33 +0300

    initial commit

---
+++ src/bin/main.rs
@@ -0,0 +1,30 @@
+//! Blinks an LED
+//!
+//! This assumes that a LED is connected to the pin assigned to `led`. (GPIO4)
+
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{
+    delay::Delay,
+    gpio::{Level, Output, OutputConfig},
+    main,
+};
+use log::info;
+
+#[main]
+fn main() -> ! {
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    esp_println::logger::init_logger_from_env();
+
+    let mut led = Output::new(peripherals.GPIO4, Level::High, OutputConfig::default());
+    let delay = Delay::new();
+
+    info!("Hello world!");
+    loop {
+        info!("Toggling LED...");
+        led.toggle();
+        delay.delay_millis(500);
+    }
+}
